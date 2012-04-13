2e93c01a0d5f6b1131dfd9a59e2ce197ad921527
Rodrigo Navarro <rnavarro1@gmail.com>
2012-04-13 15:30:16 -0300

    Extracting sum function

--- ext/standard_deviation/standard_deviation.c
+++ ext/standard_deviation/standard_deviation.c
@@ -1,25 +1,34 @@
 #include <ruby.h>
 #include <math.h>
 
-static VALUE stdev(VALUE self) {
-  int i, size;
-  double total, mean, variance;
-
-  size  = RARRAY_LEN(self);
-  total = variance = 0;
-  VALUE *array = RARRAY_PTR(self);
+double sum(VALUE *array, int size) {
+  int i;
+  double total = 0;
 
   for (i = 0; i < size; i++) {
     total += NUM2DBL(array[i]);
   }
 
-  mean = total / size;
+  return total;
+}
+
+static VALUE stdev(VALUE self) {
+  int i, size;
+  double mean, variance;
+
+  size  = RARRAY_LEN(self);
+  variance = 0;
+  VALUE *array = RARRAY_PTR(self);
+
+  mean = sum(array, size) / size;
 
   for (i = 0; i < size; i++) {
     variance += pow((NUM2DBL(array[i]) - mean), 2);
   }
 
-  return rb_float_new(sqrt(variance / (size - 1)));
+  variance = variance / (size - 1);
+
+  return rb_float_new(sqrt(variance));
 }
 
 static VALUE stdevp(VALUE self) {
