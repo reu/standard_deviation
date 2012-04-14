4a7e95f05506921209860ea0b664880d1e762105
Rodrigo Navarro <rnavarro1@gmail.com>
2012-04-14 12:12:34 -0300

    Testing against 10 decimal points.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,8 @@
 require "standard_deviation"
 require "bigdecimal"
+
+RSpec::Matchers.define :be_close_to do |expected|
+  match do |actual|
+    actual.round(10) == expected.round(10)
+  end
+end
--- spec/standard_deviation/standard_deviation_spec.rb
+++ spec/standard_deviation/standard_deviation_spec.rb
@@ -21,17 +21,17 @@ describe Array do
 
       context "with integer values" do
         let(:values) { [1, 2, 6, 3, -4, 23] }
-        it { should == 9.32559202767667 }
+        it { should be_close_to 9.32559202767667 }
       end
 
       context "with float values" do
         let(:values) { [1.0, 2.0, 6.0, 3.0, -4.0, 23.0] }
-        it { should == 9.32559202767667 }
+        it { should be_close_to 9.32559202767667 }
       end
 
       context "with bigdecimal values" do
         let(:values) { [BigDecimal("1.0"), BigDecimal("2.0"), BigDecimal("6.0"), BigDecimal("3.0"), BigDecimal("-4.0"), BigDecimal("23.0")] }
-        it { should == 9.32559202767667 }
+        it { should be_close_to 9.32559202767667 }
       end
     end
   end
@@ -43,17 +43,17 @@ describe Array do
 
     context "with integer values" do
       let(:values) { [1, 2, 6, 3, -4, 23] }
-      it { should == 8.513061859414755 }
+      it { should be_close_to 8.513061859414755 }
     end
 
     context "with float values" do
       let(:values) { [1.0, 2.0, 6.0, 3.0, -4.0, 23.0] }
-      it { should == 8.513061859414755 }
+      it { should be_close_to 8.513061859414755 }
     end
 
     context "with bigdecimal values" do
       let(:values) { [BigDecimal("1.0"), BigDecimal("2.0"), BigDecimal("6.0"), BigDecimal("3.0"), BigDecimal("-4.0"), BigDecimal("23.0")] }
-      it { should == 8.513061859414755 }
+      it { should be_close_to 8.513061859414755 }
     end
   end
 
@@ -65,17 +65,17 @@ describe Array do
 
       context "with integer values" do
         let(:values) { [1, 2, 6, 3, -4, 23] }
-        it { should == 86.96666666666665 }
+        it { should be_close_to 86.96666666666665 }
       end
 
       context "with float values" do
         let(:values) { [1.0, 2.0, 6.0, 3.0, -4.0, 23.0] }
-        it { should == 86.96666666666665 }
+        it { should be_close_to 86.96666666666665 }
       end
 
       context "with bigdecimal values" do
         let(:values) { [BigDecimal("1.0"), BigDecimal("2.0"), BigDecimal("6.0"), BigDecimal("3.0"), BigDecimal("-4.0"), BigDecimal("23.0")] }
-        it { should == 86.96666666666665 }
+        it { should be_close_to 86.96666666666665 }
       end
     end
   end
@@ -87,17 +87,17 @@ describe Array do
 
     context "with integer values" do
       let(:values) { [1, 2, 6, 3, -4, 23] }
-      it { should == 72.47222222222221 }
+      it { should be_close_to 72.47222222222221 }
     end
 
     context "with float values" do
       let(:values) { [1.0, 2.0, 6.0, 3.0, -4.0, 23.0] }
-      it { should == 72.47222222222221 }
+      it { should be_close_to 72.47222222222221 }
     end
 
     context "with bigdecimal values" do
       let(:values) { [BigDecimal("1.0"), BigDecimal("2.0"), BigDecimal("6.0"), BigDecimal("3.0"), BigDecimal("-4.0"), BigDecimal("23.0")] }
-      it { should == 72.47222222222221 }
+      it { should be_close_to 72.47222222222221 }
     end
   end
 end
